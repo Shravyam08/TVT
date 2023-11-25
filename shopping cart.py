1618fed1fcdda1aa97ab9dde8013106ab3c3425b
Shravyam08 <128493898+Shravyam08@users.noreply.github.com>
2023-11-25 13:11:10 +0530

    Add files via upload

---
+++ shopping cart.py
@@ -0,0 +1,19 @@
+def shoppingcart(gst):
+    if leatherwallet==1100:
+        gst="18%"
+    elif umbrella==900:
+        gst="12%"
+    elif cigarette==200:
+        gst="28%"
+    else: 
+        honey==100
+        gst="0%"
+    return False
+print('The item we paid maximum gst is: cigarette')
+def sum (leatherwallet,umbrella,cigarette,honey):
+    leatherwallet==1100
+    umbrella==900
+    cigarette==200
+    honey==100
+    sum=(1*leatherwallet+4*umbrella+3*cigarete+2*honey)
+print("the total amount to be paid to the shopkeeper is:",sum)
